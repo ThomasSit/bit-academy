ac8f4b82c15f0f32c5b86d3b8422daee165b762f
ThomasSit <165077@student.horizoncollege.nl>
2022-06-09 09:16:04 +0200

    ik

---
+++ PHP Level-02/name.php
@@ -0,0 +1,13 @@
+<?php
+
+/*echo "Hallo persoon";
+
+$reactie = readline();
+
+echo $reactie;*/
+
+$voornaam = readline('wat is je voornaam');
+$achternaam= readline('wat is jou achternaam');
+
+echo 'je volledige naam is' . $voornaam . $achternaam;
+
