82a888462e88bcd6e2c515f236e90f1565f31a3e
ThomasSit <165077@student.horizoncollege.nl>
2022-11-01 09:51:50 +0100

    nieuwe opdrachten

--- PHP Level-03/rijbewijs.php
+++ PHP Level-03/rijbewijs.php
@@ -1,9 +1,9 @@
 <?php  
 
-$leeftijd = readline("wat is jou leeftijd? ");
-
-if ($leeftijd <= 16){
-    echo "Helaas je mag nog niet beginnen met rijlessen";
-} else {
+$leeftijd = readline("Hoe oud ben je? ");
+   echo "Je mag beginnen met rijlessen";
+if ($leeftijd >15){
     echo "Je mag beginnen met rijlessen";
+} else if (<=15) {
+ echo "Helaas, je mag nog niet beginnen met rijlessen";
 }
