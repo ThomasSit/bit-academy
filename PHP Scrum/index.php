ed6376bba7bf3f391f147e6322540bc8bb5ae61d
ThomasSit <165077@student.horizoncollege.nl>
2023-02-23 08:34:26 +0100

    animation

--- PHP Scrum/index.php
+++ PHP Scrum/index.php
@@ -6,6 +6,14 @@ include "include/navbar.php";
 <h1>Home!</h1>
 <p>Welcome</p>
 
-
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<lottie-player src="https://assets4.lottiefiles.com/packages/lf20_jKO4EX9d7w.json"  background="transparent"  speed="1"  style="width: 500px; height: 500px;"  loop  autoplay></lottie-player>
 
 <?php include "include/footer.php"?>
+
+
+
+
+
+
+
